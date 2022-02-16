956f6c0caf83e26eacfd8f70b0f95ff7664d2726
文博 郑 <1304593623@qq.com>
2022-02-17 01:27:03 +0800

    1

--- 2.16.c
+++ 2.16.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
-#define PAGES 959
+//#define PAGES 9
+//int main(void)
+//{
+//	printf("*%d*\n", PAGES);
+//	printf("*%2d*\n", PAGES);//两个空格长度
+//	printf("*%10d*\n", PAGES);//10个空格长度
+//	printf("*%-10d*\n", PAGES);//-代表左端
+//	return 0;
+//}
+// 
+
 int main(void)
 {
-	printf("%d*\n", PAGES);
-	printf("*%2d*\n", PAGES);
-	printf("*%10d*\n", PAGES);
-	printf("*%-10d*\n", PAGES);
+	const double RENT = 3852.99;
+	printf("*%f*\n",RENT);//%f小数点后打印6位数字
+	printf("*%e*\n",RENT);
+	printf("*%4.2f*\n",RENT);
+	printf("*3.1f*\n", RENT);
+	printf("*%10.3f*\n", RENT);
+	printf("*%10.3E*\n", RENT);
+	printf("*%+4.2f*\n", RENT);
+	printf("*%010.2f*\n", RENT);
 	return 0;
 }
- 
