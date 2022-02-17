c212df5eff1cf490808bb10abaf1ff6abf21dd43
文博 郑 <1304593623@qq.com>
2022-02-17 13:36:37 +0800

    1

--- 2.16.c
+++ 2.16.c
@@ -10,16 +10,16 @@
 //}
 // 
 
-int main(void)
-{
-	const double RENT = 3852.99;
-	printf("*%f*\n",RENT);//%f小数点后打印6位数字
-	printf("*%e*\n",RENT);
-	printf("*%4.2f*\n",RENT);
-	printf("*3.1f*\n", RENT);
-	printf("*%10.3f*\n", RENT);
-	printf("*%10.3E*\n", RENT);
-	printf("*%+4.2f*\n", RENT);
-	printf("*%010.2f*\n", RENT);
-	return 0;
-}
+//int main(void)
+//{
+//	const double RENT =3852.99;
+//	printf("*%f*\n",RENT);//%f小数点后打印6位数字
+//	printf("*%e*\n",RENT);//%e表示在小数点左侧打印一个 右侧打印6个数字
+//	printf("*%4.2f*\n",RENT);
+//	printf("*3.1f*\n", RENT);
+//	printf("*%10.3f*\n", RENT);
+//	printf("*%10.3E*\n", RENT);
+//	printf("*%+4.2f*\n", RENT);//多打印一个+号
+//	printf("*%010.2f*\n", RENT);
+//	return 0;
+//}
