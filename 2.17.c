5cdbcabe62c032c3dda7d645c0b0387175174039
文博 郑 <1304593623@qq.com>
2022-02-17 23:42:50 +0800

    1

--- 2.17.c
+++ 2.17.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
-int main(void)
-{
-	printf("%x %X %#x\n", 31,31,31);
-	printf("**%d**% d**% d**\n", 42, 42, -42);
-	printf("**%5d**%5.3d**%05d**%05.3d**\n", 6, 6, 6, 6);
-	return 0;
-}
+//int main(void)
+//{
+//	printf("%x %X %#x\n", 31,31,31);//lf是16进制数 在十进制中表示31
+//	printf("**%d**% d**% d**\n", 42, 42, -42);//正值前面可以导入空格 负值前面不可以导入空格
+//	printf("**%5d**%5.3d**%05d**%05.3d**\n", 6, 6, 6, 6);//?
+//	return 0;
+//}
+
+//#define a "austhentic imitation"
+//int main(void)
+//{
+//	printf("%2s\n", a);
+//	printf("%24\n", a);
+//	printf("%24.5\n", a);
+//	printf("%-24.5\n", a);     ?
+//	return 0;
+//}
+
