8b65f70bf1d9ab0f20313870b45d69014584a8a0
文博 郑 <1304593623@qq.com>
2022-02-19 11:50:21 +0800

    1

---
+++ 猜数游戏.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//int main()
+//{
+//	srand(time(0));
+//	int number = rand() % 100 + 1;//rand是取一个随机数
+//	int count = 0;
+//	int a = 0;
+//	printf("我想好一个数了");
+//	do {
+//		printf("请猜一个数");
+//		scanf_s("%d", &a);
+//		count++;
+//		if (a > number) {
+//			printf("你猜的数大了");
+//		}
+//		else if (a < number) {
+//			printf("你猜的数小了");
+//		}//if     else if循环
+//
+//	} while (a != number);//do while循环
+//	printf("你用了%d次就对了\n", count);
+//	return 0;
+//
+//}
