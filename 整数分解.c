9f1c6f1d0d2aa8f43509b30d5feb40b18c491460
文博 郑 <1304593623@qq.com>
2022-02-22 00:42:13 +0800

    1

--- 整数分解.c
+++ 整数分解.c
@@ -8,17 +8,17 @@
 //	while (t > 9) {
 //		t /= 10;
 //		mask *= 10;
-//	}
+//	}//通过x来得到mask
 //	printf("x=%d,mask=%d\n", x, mask);
 //	do {
-//		int d = x / mask;
+//		int d = x / mask;//通过d来取x的个位数
 //		printf("%d", d);
 //		if (mask > 9) {
 //			printf(" ");
-//		}
+//		}//通过mask控制空格数量
 //		x %= mask;
 //		mask /= 10;
-//	} while (mask > 0);
+//	} while (mask > 0);//判断mask数量大小
 //	printf("\n");
 //	return 0;
 //}
