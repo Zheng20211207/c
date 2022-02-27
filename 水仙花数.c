f73ffecaafbe695335174189ba4e44119d59d0f3
文博 郑 <1304593623@qq.com>
2022-02-27 21:47:14 +0800

    1

---
+++ 水仙花数.c
@@ -0,0 +1,29 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int n=3;
+//	//scanf_s("%d", &n);
+//	int i=1;
+//	int first=1;
+//	
+//	while (i < n) {
+//		first *= 10;
+//		i++;
+//	}//printf("%d", first);
+//
+//	while (i<first < first * 10) {
+//		int t = i;
+//		int sum = 0;
+//		do {
+//			int d = t % 10;
+//			t /= 10;
+//			int p = d;
+//			int j = 0;
+//			while (j < n) {
+//				p *= d;
+//				j++;
+//			}
+//		} while (t > 0);
+//	}
+//	return 0;
+//}
